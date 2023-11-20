baf86f225269f35fa4c87d0d00b24bbb8649ead3
McLizzerson <liesbeth.fr@gmail.com>
2023-11-20 17:41:00 +0100

    create: index.js and add recipe router

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+
+import recipeRouter from "./back-end/src/routes/recipies.js";
+
+const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.use("/recipies", recipeRouter);
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000, yay");
+});
