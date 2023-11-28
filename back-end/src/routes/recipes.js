dc1bfda9163924b7cf6646ddea4a26ba21d0f157
McLizzerson <liesbeth.fr@gmail.com>
2023-11-28 17:01:05 +0100

    add: update and delete to recipe router

--- back-end/src/routes/recipes.js
+++ back-end/src/routes/recipes.js
@@ -3,6 +3,8 @@ import express from "express";
 import getRecipes from "../services/recipes/getRecipes.js";
 import getRecipeById from "../services/recipes/getRecipeById.js";
 import createRecipe from "../services/recipes/createRecipe.js";
+import updateRecipeById from "../services/recipes/updateRecipeById.js";
+import deleteRecipe from "../services/recipes/deleteRecipe.js";
 
 const recipeRouter = express.Router();
 
@@ -39,6 +41,40 @@ recipeRouter.post("/", async (req, res, next) => {
     };
     const newRecipe = await createRecipe(recipeData);
     res.status(201).json(newRecipe);
+  } catch (error) {
+    next(error);
+  }
+});
+
+recipeRouter.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, description, ingredients, image, source, categories } =
+      req.body;
+    const recipeData = {
+      name,
+      description,
+      ingredients,
+      image,
+      source,
+      categories,
+    };
+    const updatedRecipe = await updateRecipeById(id, recipeData);
+    res.status(200).json(updatedRecipe);
+  } catch (error) {
+    next(error);
+  }
+});
+
+recipeRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedRecipe = await deleteRecipe(id);
+    res
+      .status(200)
+      .json({
+        message: `Recipe with id ${deletedRecipe} was deleted succesfully.`,
+      });
   } catch (error) {
     next(error);
   }
