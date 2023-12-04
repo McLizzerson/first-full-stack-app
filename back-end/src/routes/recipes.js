05529ef84a597d470260af3cbe736977da5df37f
McLizzerson <liesbeth.fr@gmail.com>
2023-12-04 12:01:58 +0100

    add: not found error handler to recipe route

--- back-end/src/routes/recipes.js
+++ back-end/src/routes/recipes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import notFoundErrorHandler from "../middleware/notFoundErrorHandler.js";
 
 import getRecipes from "../services/recipes/getRecipes.js";
 import getRecipeById from "../services/recipes/getRecipeById.js";
@@ -17,15 +18,19 @@ recipeRouter.get("/", async (req, res, next) => {
   }
 });
 
-recipeRouter.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const recipe = await getRecipeById(id);
-    res.status(200).json(recipe);
-  } catch (error) {
-    next(error);
-  }
-});
+recipeRouter.get(
+  "/:id",
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const recipe = await getRecipeById(id);
+      res.status(200).json(recipe);
+    } catch (error) {
+      next(error);
+    }
+  },
+  notFoundErrorHandler
+);
 
 recipeRouter.post("/", async (req, res, next) => {
   try {
@@ -46,35 +51,43 @@ recipeRouter.post("/", async (req, res, next) => {
   }
 });
 
-recipeRouter.put("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { name, description, ingredients, image, source, category } =
-      req.body;
-    const recipeData = {
-      name,
-      description,
-      ingredients,
-      image,
-      source,
-      category,
-    };
-    const updatedRecipe = await updateRecipeById(id, recipeData);
-    res.status(200).json(updatedRecipe);
-  } catch (error) {
-    next(error);
-  }
-});
+recipeRouter.put(
+  "/:id",
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { name, description, ingredients, image, source, category } =
+        req.body;
+      const recipeData = {
+        name,
+        description,
+        ingredients,
+        image,
+        source,
+        category,
+      };
+      const updatedRecipe = await updateRecipeById(id, recipeData);
+      res.status(200).json(updatedRecipe);
+    } catch (error) {
+      next(error);
+    }
+  },
+  notFoundErrorHandler
+);
 
-recipeRouter.delete("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const deletedRecipe = await deleteRecipe(id);
-    res.status(200).json({
-      message: `Recipe with id ${deletedRecipe} was deleted succesfully.`,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+recipeRouter.delete(
+  "/:id",
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const deletedRecipe = await deleteRecipe(id);
+      res.status(200).json({
+        message: `Recipe with id ${deletedRecipe} was deleted succesfully.`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+  notFoundErrorHandler
+);
 export default recipeRouter;
