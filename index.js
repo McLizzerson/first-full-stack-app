308a52d95657c9e20bc446b3bf6e483e79dd4246
McLizzerson <liesbeth.fr@gmail.com>
2023-12-04 12:01:33 +0100

    add: general error handler to index.js

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ import express from "express";
 import "dotenv/config";
 
 import recipeRouter from "./back-end/src/routes/recipes.js";
+import errorHandler from "./back-end/src/middleware/errorHandler.js";
 
 const app = express();
 
+// Main body of app
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
 app.use("/recipies", recipeRouter);
+
+// Error handling
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000, yay");
